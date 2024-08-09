887d6f417ae04ef9a1040d01c3a9e71a054574c8
JosephThien2000 <n.t.m.thien20.04@gmail.com>
2024-08-09 13:23:46 +0700

    khởi tạo project và cài đặt các dependencies

---
+++ Nhom04MidTest/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Nhom04MidTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
